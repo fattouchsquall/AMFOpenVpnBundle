1bc5a6ffc7278ee28d73ca51aa420aff49f1acbd
Amine Fattouch <amine.fattouch@gmail.com>
2014-12-02 13:27:17 +0100

    Add functional tests

--- Tests/Controller/ManagementControllerTest.php
+++ Tests/Controller/ManagementControllerTest.php
@@ -1,17 +1,35 @@
 <?php
 
+/**
+ * Functional test for the controller of server management.
+ * 
+ * @package AMFOpenVpnBundle
+ * @subpackage Controller
+ * @author Mohamed Amine Fattouch <amine.fattouch@gmail.com>
+ */
 namespace AMF\OpenVpnBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Functional test for the controller of server management.
+ * 
+ * @package AMFOpenVpnBundle
+ * @subpackage Controller
+ * @author Mohamed Amine Fattouch <amine.fattouch@gmail.com>
+ */
 class ManagementControllerTest extends WebTestCase
 {
-    public function testIndex()
+    
+    /**
+     * Test the action of retrieve infos of server.
+     */
+    public function testRetrieveInfosOfServer()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $$client->request('GET', '/serveur/info/1');
+        
+        $this->assertTrue($client->getResponse()->isOk());
     }
 }
